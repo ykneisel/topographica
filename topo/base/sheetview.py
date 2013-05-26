492551a85b21dfd38d398ac4b60db2cf7367c702
jlstevens <jlstevens[at]ed.ac.uk>
2013-05-26 18:51:42 +0100

    SheetViews now used updated DataViews and can now support older snapshots

--- topo/base/sheetview.py
+++ topo/base/sheetview.py
@@ -9,19 +9,19 @@ for a single sheet, and a view can be passed around independent of the
 originating source object.
 """
 
-try:      from imagen.dataviews import Cartesian2D
+try:      from imagen.dataviews import Cartesian2Dx
 except:   raise Exception("Imagen submodule needs to be updated - please run `git submodule update' and try again.")
 
 try:
     from collections import OrderedDict
-    Cartesian2D.map_type = OrderedDict
 except ImportError:
     from topo.misc.odict import OrderedDict
-    Cartesian2D.map_type = OrderedDict
+except ImportError:
+    OrderedDict = None
 
 import param
 
-class SheetView(Cartesian2D):
+class SheetView(Cartesian2Dx):
     """
     Class provided for backward compatibility with earlier SheetView
     component.
@@ -40,13 +40,27 @@ class SheetView(Cartesian2D):
 
     def __init__(self, (data, bounds), src_name=None, precedence=0.0,
                  timestamp=-1, row_precedence=0.5, **kwargs):
-        super(SheetView,self).__init__(data, bounds,
-                                       src_name=src_name,
+
+        self.map_type = OrderedDict
+        super(SheetView,self).__init__(bounds, src_name=src_name,
                                        precedence=precedence,
                                        timestamp = timestamp,
                                        row_precedence = row_precedence,
                                        **kwargs)
+        self.record(data.copy(), timestamp)
 
+
+    def view(self):
+        """
+        Return the requested view as a (data, bbox) tuple.  Provided
+        for backward compatibility with the original Topographica
+        SheetView model. It is now easier to access the data and
+        bounds attributes directly.
+        """
+        if hasattr(self, '_data'):
+            return (self._data[-1], self.bounds)
+        else: # Necessary for backward compatibility with older snapshots.
+            return self._view_list[0]
 
 
 def UnitView((data, bounds), x, y, projection, timestamp, **params):
